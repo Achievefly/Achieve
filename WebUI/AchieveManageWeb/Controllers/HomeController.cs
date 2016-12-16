f2b80cc7837457b9c3336e131f2730aa43bb0ec0
x952897642 <952896642@qq.com>
2016-12-16 18:13:27 +0800

    修改主页用户名称显示

--- WebUI/AchieveManageWeb/Controllers/HomeController.cs
+++ WebUI/AchieveManageWeb/Controllers/HomeController.cs
@@ -21,13 +21,6 @@ namespace AchieveManageWeb.Controllers
 
         public ActionResult Index()
         {
-            Sys_User uInfo = ViewData["Account"] as Sys_User;
-            if (uInfo == null)
-            {
-                return RedirectToAction("Index", "Login");
-            }
-            ViewBag.RealName = uInfo.F_RealName;
-            ViewBag.TimeView = DateTime.Now.ToLongDateString();
             return View();
         }
 
